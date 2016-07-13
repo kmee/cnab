c8922eb716fa2c2b6fd2247f9110919a0f485133
Carlos R. Silveira <crsilveira@gmail.com>
2016-07-13 15:13:29 -0300

    Removido espaços e alguns ajustes.

--- cnab240/errors.py
+++ cnab240/errors.py
@@ -11,7 +11,8 @@ class AtribuicaoCampoError(Cnab240Error):
     def __init__(self, campo, valor):
         self.campo = campo
         self.valor = valor
-        super(AtribuicaoCampoError, self).__init__(self)
+        #super(AtribuicaoCampoError, self).__init__(self)
+        super(AtribuicaoCampoError, self).__init__()
 
     def __unicode__(self):
         return u'campo:{0} formato:{1} decimais:{2} digitos:{3} - valor:{4}'.\
@@ -23,10 +24,18 @@ class AtribuicaoCampoError(Cnab240Error):
                 repr(self.valor),
             )
 
+    def __str__(self):
+        return 'campo:{0} formato:{1} decimais:{2} digitos:{3} - valor:{4}'.\
+            format(
+                self.campo.nome,
+                self.campo.formato,
+                self.campo.decimais,
+                self.campo.digitos,
+                repr(self.valor),
+            )
 
 class NumDigitosExcedidoError(AtribuicaoCampoError):
     """Tentativa de atribuicao de valor mais longo que o campo suportaia"""
-
 
 class TipoError(AtribuicaoCampoError):
     """Tentativa de atribuicao de tipo nao suportado pelo campo"""
@@ -41,7 +50,8 @@ class FaltandoArgsError(Cnab240Error):
 
     def __init__(self, args_faltantes):
         self.args_faltantes = args_faltantes
-        super(FaltandoArgsError, self).__init__(self)
+        #super(FaltandoArgsError, self).__init__(self)
+        super(FaltandoArgsError, self).__init__()
 
     def __unicode__(self):
         return (u'Os seguintes kwargs sao obrigatorios e nao foram '
