6a0fdcff398dbe230ec1b52c0d6d22cd127a57fd
Luis Felipe Mileo <mileo@kmee.com.br>
2017-07-08 00:53:19 -0300

    [REF] Flake 8 tests

--- tests/test_arquivo.py
+++ tests/test_arquivo.py
@@ -1,7 +1,7 @@
 
-try:                                                                             
+try:
     import unittest2 as unittest
-except ImportError:                                                              
+except ImportError:
     import unittest
 
 import os
@@ -11,14 +11,14 @@ from cnab240 import errors
 from cnab240.bancos import itau
 from cnab240.tipos import Arquivo
 from tests.data import get_itau_data_from_dict, get_itau_file_remessa, \
-                                                                ARQS_DIRPATH
+    ARQS_DIRPATH
 
 
 class TestCnab240(unittest.TestCase):
-    
+
     def __init__(self, *args, **kwargs):
         super(TestCnab240, self).__init__(*args, **kwargs)
-        self.maxDiff = None 
+        self.maxDiff = None
 
     def setUp(self):
         self.itau_data = get_itau_data_from_dict()
