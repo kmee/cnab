74b7d8e9cda4fbdd0daa55a7a752994f8f86a262
Luis Felipe Mileo <mileo@kmee.com.br>
2017-07-08 01:46:13 -0300

    [REM] Classe não utilizada

--- cnab240/lotes/cobranca.py
+++ cnab240/lotes/cobranca.py
@@ -3,5 +3,6 @@ from cnab240.tipos import LoteBase
 
 
 class LoteCobranca(LoteBase):
-    HeaderCls = LoteCobranca.banco.registros.HeaderLoteCobranca
-    TrailerCls = LoteCobranca.banco.registros.TrailerLoteCobranca
+    pass
+    # HeaderCls = LoteCobranca.banco.registros.HeaderLoteCobranca
+    # TrailerCls = LoteCobranca.banco.registros.TrailerLoteCobranca
