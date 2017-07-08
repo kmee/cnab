36eceeb643054007ae5fe265952d3f09ee82cbe8
Luis Felipe Mileo <mileo@kmee.com.br>
2017-07-08 01:02:02 -0300

    [REF] pep8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     ],
     license='MIT',
     description='Classe para gerar arquivo de remessa e leitura de retorno no '
-                                                            'padrão CNAB',
+    'padrão CNAB240',
     long_description=open('README.md', 'r').read(),
     download_url='https://github.com/kmee/cnab',
     scripts=[],
